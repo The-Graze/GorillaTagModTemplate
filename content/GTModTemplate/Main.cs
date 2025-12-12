b1852a8a0530b556ecf0730e26e28eaa7db5bf36
Bingus of the Forest <bendawsdev@gmail.com>
2025-12-12 08:57:43 -0600

    Final changes

--- content/GTModTemplate/Main.cs
+++ content/GTModTemplate/Main.cs
@@ -1,13 +1,3 @@
-/*
- * Hey all, welcome to the mod template I use for like everything :)
- * It contains many utilities to start working on mods without the need to setup anything.
- *
- * Browse through the included directories to see what you have to work with.
- * The methods commented below in the code are significant.
- *
- *      - Bingus
-*/
-
 using GTModTemplate.Patches;
 using GTModTemplate.Utilities;
 using BepInEx;
@@ -24,15 +14,18 @@ public class Main : BaseUnityPlugin
     // The starting point of the mod. This methodd is called when the mod is loaded.
     private void Start()
     {
-        Instance = this;
+        Instance ??= this;
         HarmonyPatches.Patch();
         
         // Here is the spot to load any Asset Bundles in your program.
+
+        // MethodUtilities.Attempt is a wrapper for the try block, we use it
+        // here to prevent your mod from breaking every other mod in the case
+        // that your OnPlayerSpawned() method causes errors.
         GorillaTagger.OnPlayerSpawned(() => MethodUtilities.Attempt(OnPlayerSpawned));
         
         // This would be the spot to unload all of your AssetBundles when your
-        // assets have been loaded in.
-        // Save memory!
+        // assets have been loaded in. Save some memory!
         AssetBundleUtilities.FreeCache();
     }
 
